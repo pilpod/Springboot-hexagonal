d487d38abc646436ddf9f82343aa349240ec8611
Giacomo <giacomo.corso@gmail.com>
2023-06-17 19:34:33 +0200

    inputAdapters.http CustomerAPI and OrderAPI

---
+++ src/main/java/dev/giacomo/hexagonal/customers/infrastructure/inputAdapters/http/CustomerAPI.java
@@ -0,0 +1,37 @@
+package dev.giacomo.hexagonal.customers.infrastructure.inputAdapters.http;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import dev.giacomo.hexagonal.customers.domain.Customer;
+import dev.giacomo.hexagonal.customers.infrastructure.inputPorts.CustomerInputPort;
+
+@RestController
+@RequestMapping(value = "customer")
+public class CustomerAPI {
+
+    @Autowired
+    CustomerInputPort customerInputPort;
+
+    @PostMapping(value = "create", produces=MediaType.APPLICATION_JSON_VALUE)
+    public Customer create( @RequestParam String name, @RequestParam String country ) {
+        return customerInputPort.createCustomer(name, country);
+        }
+
+    @PostMapping(value = "get", produces=MediaType.APPLICATION_JSON_VALUE)
+    public Customer get( @RequestParam Long customerId ) {
+        return customerInputPort.getById(customerId);
+        }
+
+    @PostMapping(value = "getall", produces=MediaType.APPLICATION_JSON_VALUE)
+    public List<Customer> getAll() {
+        return customerInputPort.getAll();
+        }
+           
+}
---
+++ src/main/java/dev/giacomo/hexagonal/orders/infrastructure/inputAdapters/http/OrderAPI.java
@@ -0,0 +1,27 @@
+package dev.giacomo.hexagonal.orders.infrastructure.inputAdapters.http;
+
+import java.math.BigDecimal;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import dev.giacomo.hexagonal.orders.domain.Order;
+import dev.giacomo.hexagonal.orders.infrastructure.inputPorts.OrderInputPort;
+
+@RestController
+@RequestMapping(value = "order")
+public class OrderAPI {
+    
+    @Autowired
+    OrderInputPort orderInputPort;
+
+    @PostMapping(value = "create", produces=MediaType.APPLICATION_JSON_VALUE)
+    public Order create( @RequestParam Long customerId, @RequestParam BigDecimal total ) {
+        return orderInputPort.createOrder(customerId, total);
+        }
+    
+}
